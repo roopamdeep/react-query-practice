cfe8a2ccb6b1bf64aaabf0074e4a8b8c598db28e
roopamdeep <roopam+kaur@yahoo.in>
2024-05-05 15:28:34 -0400

    Initial commit

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 interface Todo {
   id: number;
